34ff177a2621d67eb4e7e98d277ab12187d3dddb
rafacabrera7 <rafael.cabrera@urosario.com>
2021-05-04 17:15:39 -0500

    Nuevos histogramas

--- Code1.R
+++ Code1.R
@@ -2,19 +2,19 @@
 #Importando los datos de excel a un dataset
 #
 library(readxl) 
-COPUSD <- read_excel("C:/Users/USUARIO/Desktop/COPUSD prices.xlsx", 
+COPUSD <- read_excel("C:/Users/rafae/Desktop/Probabilidad/ProyectoProba/COPUSD prices.xlsx", 
                      sheet = "COPUSD")
 View(COPUSD)
 
 library(readxl)
-COPUSD_prices <- read_excel("C:/Users/USUARIO/Desktop/COPUSD prices.xlsx", 
+COPUSD_prices <- read_excel("C:/Users/rafae/Desktop/Probabilidad/ProyectoProba/COPUSD prices.xlsx", 
                             sheet = "brent", col_types = c("date", 
                                                            "numeric"))
 View(COPUSD_prices)
 
 
 library(readxl)
-Coffe <- read_excel("C:/Users/USUARIO/Desktop/COPUSD prices.xlsx", 
+Coffe <- read_excel("C:/Users/rafae/Desktop/Probabilidad/ProyectoProba/COPUSD prices.xlsx", 
                     sheet = "Coffe", col_types = c("date", 
                                                    "numeric"))
 View(Coffe)
--- histogramas.R
+++ histogramas.R
@@ -1,2 +1,42 @@
+#############################################################################################################################
+#Importando los datos de excel a un dataset
+#OJO se deben poner las direcciones donde tenga el archivo en su equipo
+library(readxl)
+COPUSD <- read_excel("C:/Users/USUARIO/Desktop/COPUSD prices.xlsx",
+                     sheet = "COPUSD")
+View(COPUSD)
 
-hist(dplyr::pull(cop_usd, "Close"), main ="Histograma precios del dólar", xlab = "Precio de cierre", ylab = "Frecuencia",col = "darkgreen")
+library(readxl)
+COPUSD_prices <- read_excel("C:/Users/USUARIO/Desktop/COPUSD prices.xlsx",
+                            sheet = "brent", col_types = c("date",
+                                                           "numeric"))
+View(COPUSD_prices)
+
+
+library(readxl)
+Coffe <- read_excel("C:/Users/USUARIO/Desktop/COPUSD prices.xlsx",
+                    sheet = "Coffe", col_types = c("date",
+                                                   "numeric"))
+View(Coffe)
+
+
+#Fin de la importaciÃ³n
+#############################################################################################################################
+
+
+#############################################################################################################################
+#Histograma
+
+#Es necesario instalar install.packages("dplyr", dependencies = T)
+
+library("dplyr")
+hist(COPUSD_prices %>% pull (Close), main ="Histograma precios del dï¿½lar", xlab = "Precio de cierre", ylab = "Frecuencia",col = "darkgreen")
+hist(brent %>% pull (Close), main ="Histograma precios del petrï¿½leo Brent", xlab = "Precio de cierre (en dï¿½lares)", ylab = "Frecuencia",col = "darkgrey")
+hist(coffee %>% pull (Close), main ="Histograma precios del Cafï¿½", xlab = "Precio de cierre (en dï¿½lares)", ylab = "Frecuencia",col = "darkgrey")
+
+#############################################################################################################################
+#Diagrama de Caja
+
+
+
+#############################################################################################################################
