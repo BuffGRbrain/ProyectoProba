f54b14f0ed6f202e5f821e23534f93ede4d6028a
cmilo22 <camiloa.silva@urosario.edu.co>
2021-05-11 16:52:28 -0500

    graficas corregidas

--- CodigosR/graficas.R
+++ CodigosR/graficas.R
@@ -8,15 +8,15 @@ View(COPUSD_prices)
 
 library(readxl)
 brent <- read_excel("probabilidadyestadistica/ProyectoProba/COPUSD prices.xlsx",
-                            sheet = "brent", col_types = c("date",
-                                                           "numeric"))
+                    sheet = "brent", col_types = c("date",
+                                                   "numeric"))
 View(brent)
 
 
 library(readxl)
 Coffee <- read_excel("probabilidadyestadistica/ProyectoProba/COPUSD prices.xlsx",
-                    sheet = "Coffe", col_types = c("date",
-                                                   "numeric"))
+                     sheet = "Coffe", col_types = c("date",
+                                                    "numeric"))
 View(Coffee)
 #Fin de la importación
 #############################################################################################################################
@@ -28,10 +28,9 @@ View(Coffee)
 #Es necesario instalar install.packages("dplyr", dependencies = T)
 
 library("dplyr")
-hist(COPUSD_prices %>% pull (Close), main ="Histograma precios del dolar", xlab = "Precio de cierre", ylab = "Frecuencia",col = "darkgreen", breaks=5)
-hist(brent %>% pull (Close), main ="Histograma precios del petroleo Brent", xlab = "Precio de cierre (en dolares)", ylab = "Frecuencia",col = "darkgrey", breaks=5)
-hist(Coffee %>% pull (Close), main ="Histograma precios del Cafe", xlab = "Precio de cierre (en dolares)", ylab = "Frecuencia",col = 623, breaks=5)
-
+hist(COPUSD_prices %>% pull (Close), main ="Histograma precios del dolar", xlab = "Precio de cierre", ylab = "Frecuencia",col = "darkgreen",breaks=seq(0,5000, by=600))
+hist(brent %>% pull (Close), main ="Histograma precios del petroleo Brent", xlab = "Precio de cierre (en dolares)", ylab = "Frecuencia",col = "darkgrey",breaks=seq(0,160, by=29))
+hist(Coffee %>% pull (Close), main ="Histograma precios del Cafe", xlab = "Precio de cierre (en dolares)", ylab = "Frecuencia",col = 623, breaks=seq(10,360, by=50))
 
 #############################################################################################################################
 #Diagrama de Caja
@@ -51,8 +50,8 @@ Coffee <- coffee # %>% (Close)
 brent <- brent # %>% (Close)
 
 preciosdol = plot(COPUSD, type="l", main="Precios del dolar", xlab = "Fecha", ylab = "Precio de cierre", col="darkgreen")
-coffeee = plot(Coffee, type="l", main="Precios del cafe", xlab = "Fecha", ylab = "Precio de cierre", col=623)
-brentt = plot(brent, type="l", main="Precios del petroleo", xlab = "Fecha", ylab = "Precio de cierre", col="darkgrey")
+coffeee = plot(Coffee, type="l", main="Precios del cafe", xlab = "Fecha", ylab = "Precio de cierre", col="darkgrey")
+brentt = plot(brent, type="l", main="Precios del petroleo", xlab = "Fecha", ylab = "Precio de cierre", col=623)
 
 
 #############################################################################################################################
