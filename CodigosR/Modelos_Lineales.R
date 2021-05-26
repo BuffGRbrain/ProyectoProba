eecbe23803da4915948dfc436cf8c20acf74c319
rafacabrera7 <rafael.cabrera@urosario.com>
2021-05-26 17:19:09 -0500

    PH listas

--- CodigosR/Modelos_Lineales.R
+++ CodigosR/Modelos_Lineales.R
@@ -2,7 +2,7 @@
 ### Modelo de regresión lineal para Brent(x) y el dolar(y)
 x = BRENT_ds$Close
 y = COPUSD_ds$Close
-
+n = length(x)
 mod_brent = lm(y~x)
 summary(mod_brent)
 
@@ -10,71 +10,45 @@ plot(x,y, xlab = "Precio Brent (USD)", ylab = "Precio Dolar (COP)")
 
 abline(mod_brent, col = 2, lwd = 3)
 
-# B0 = 4019.1269 Intercepto
-# B1 = -19.8993 Pendiente
-# Correlación: 0.6631 ed 66.31% Podemos decir que el precio del dolar se ve muy influenciado por el precio del petroleo brent que exporta colombia
+#Datos muestrales (Tomados a partir del modelo)
+# B0 = 4019.0048 Intercepto
+# B1 = -19.8962 Pendiente
+#r^2 = 0.663
+# Correlación: 0.6631 ed 66.31% Podemos decir que el precio del dolar se ve muy
+#influenciado por el precio del petroleo brent que exporta colombia
 # Pruebas de hipotesis
 
-###Intervalos de confianza y prediccion
 
+#Prueba de hippótesis para probar B1 = 0
+alpha = 0.05
+#H0: B1 = 0
+#Ha: B1 != 0
+#T = -79.47
+T_alpha_medios = qt(alpha/2, lower.tail =  FALSE, df = n-2)
+#T_alpha_medios = 1.96
+#Dado que |T| > T_alpha_medios, rechazamos H0, por lo tanto, existe una
+#Correlación entre el precio del petroleo Brent y el precio del dólar con
+#respecto al peso col con un nivel de significación del 0.05.
+#vp < 2e-16
+
+###Intervalos de confianza y prediccion
 #Intervalo de confianza del 90% cuando el precio del brent(x) es igual a 50
 x_n = data.frame(x=50)
 predict(mod_brent, x_n, level=0.9, interval="confidence")
-# IC = [3008.497, 3039.823]
+# IC = [3008.529, 3039.862]
 
 #Intervalo de prediccion del 90% cuando el precio del brent(x) es igual a 50
 x_n = data.frame(x=50)
 predict(mod_brent, x_n, level=0.9, interval="prediction")
-#IP = [2394.5, 3653.82]
+#IP = [2394.392, 3653.999]
 
-
-########################################################################################################################################################################################################
-xb = mean(x)
-yb= mean(y)
-
-Sxx = sum((x-xb)^2)
-Sxy = sum((x-xb)*(y-yb))
-Syy = sum((y-yb)^2)
-
-
-
-B1 = Sxy/Sxx # pendiente de la recta
-B0 = yb - B1*xb #ptos de corte con observaciones
-
-#Prueba de hipótesis para E[Y] para saber como es el error de estimación Pendiente
-#Prueba de hippótesis para probar B1 = 0
-#H0: B1 = 0
-#Ha: B1 != 0
-
-n = length(x)
-B1_0 = 0
-SSE = Syy - B1*Sxy
-
-#Desviación estandard
-S = sqrt(SSE/(n-2))
-S_1 = sqrt(S)
-c11 = 1/Sxx
-
-T = (B1 - B1_0/(S*sqrt(c11))) # Estadístico de prueba
-
-alpha = 0.05
-
-T_alpha = qt(alpha/2, lower.tail =  FALSE, df = n-2) # para el valor de Talfa
-#luego T cae en la región de rechazo, es decir B1 es distinto de 0
-valorp = pt(T, lower.tail =  FALSE, df = n-2) #pt es para valor p # Revisar pues no se si esta bien
-
-#Intervalo de confianza para B1
-#confint lifehack
-limsup = B1+T_alpha *( S_1 * sqrt(c11))
-liminf = B1-T_alpha *( S_1 * sqrt(c11))
-#Luego concluimos que B1 tiene un valor negativo
 
 ############################################################################################################################################################################################################################
 #####Regresion lineal café
 
 x_Coffee = COFFEE_ds$Close
 y_Coffee = COPUSD_ds$Close
-
+n = length(x_Coffee)
 mod_coffee = lm(y_Coffee~x_Coffee)
 summary(mod_coffee)
 
@@ -82,59 +56,39 @@ plot(x_Coffee,y_Coffee, xlab = "Precio Coffee (USD)", ylab = "Precio Dolar (COP)
 
 abline(mod_coffee, col = 2, lwd = 3)
 
-# B0 =  Intercepto
-# B1 =  Pendiente
+#Datos muestrales (Tomados a partir del modelo)
+# B0 = 3819.0144 Intercepto
+# B1 = -9.2038 Pendiente
+# r^2 = 0.3399
 # Correlación:  Podemos decir que el precio del dolar se ve muy influenciado por el precio del petroleo brent que exporta colombia
 # Pruebas de hipotesis
+
+#Prueba de hippótesis para probar B1 = 0
+alpha = 0.05
+#H0: B1 = 0
+#Ha: B1 != 0
+#T = -40.66
+T_alpha_medios = qt(alpha/2, lower.tail =  FALSE, df = n-2)
+#T_alpha_medios = 1.96
+#Dado que |T| > T_alpha_medios, rechazamos H0, por lo tanto, existe una
+#Correlación entre el precio del Café y el precio del dólar con
+#respecto al peso col con un nivel de significación del 0.05.
+#vp < 2e-16
 
 ###Intervalos de confianza y prediccion
 
 #Intervalo de confianza del 90% cuando el precio del brent(x) es igual a 50
 x_Coffee_n = data.frame(x_Coffee=50)
 predict(mod_coffee, x_Coffee_n, level=0.9, interval="confidence")
-# IC = [3008.497, 3039.823]
+# IC = [3320.402, 3297.241]
 
 #Intervalo de prediccion del 90% cuando el precio del brent(x) es igual a 50
 x_n = data.frame(x=50)
 predict(mod_coffee, x_n, level=0.9, interval="prediction")
-#IP = [2394.5, 3653.82]
-
+#IP = [2476.867, 4220.777]
 
 ############################################################################################################################################################################################################################
-x_Coffeeb = mean(x_Coffee)
-y_Coffeeb= mean(y_Coffee)
-
-Sxx = sum((x_Coffee-x_Coffeeb)^2)
-Sxy = sum((x_Coffee-x_Coffeeb)*(y_Coffee-y_Coffeeb))
-Syy = sum((y_Coffee-y_Coffeeb)^2)
-
-B1 = Sxy/Sxx # pendiente de la recta
-B0 = y_Coffeeb - B1*x_Coffeeb #ptos de corte con observaciones
-
-#Prueba de hipótesis para E[Y] para saber como es el error de estimación Pendiente
-#Prueba de hippótesis para probar B1 = 0
-#H0: B1 = 0
-#Ha: B1 != 0
-
-n = length(x_Coffee)
-B1_0 = 0
-SSE = Syy - B1*Sxy
-
-#Desviación estandard
-S = sqrt(SSE/(n-2))
-S_1 = sqrt(S)
-c11 = 1/Sxx
-
-T = (B1 - B1_0/(S*sqrt(c11))) # Estadístico de prueba
-
-alpha = 0.05
-
-T_alpha = qt(alpha/2, lower.tail =  FALSE, df = n-2) # para el valor de Talfa
-#luego T cae en la región de rechazo, es decir B1 es distinto de 0
-valorp = pt(T, lower.tail =  FALSE, df = n-2) #pt es para valor p # Revisar pues no se si esta bien
-
-#Intervalo de confianza para B1
-#confint lifehack
-limsup = B1+T_alpha *( S_1 * sqrt(c11))
-liminf = B1-T_alpha *( S_1 * sqrt(c11))
-#Luego concluimos que B1 tiene un valor negativo
+#en caso de querer hacerlo partiendo a partir de cierto valor
+#BRENT_COPUSD=merge(BRENT_ds, COPUSD_ds, by="Date" ) #Union del modelo para ver como se comporta por intervalos
+#BRENT_COPUSD=merge(BRENT_ds, COPUSD_ds, by="Date")
+#BRENT_COPUSD<-BRENT_COPUSD[BRENT_COPUSD$Close_BRENT > 79]
